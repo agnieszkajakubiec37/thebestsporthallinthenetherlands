adda2b4c9a17a52b512232695cab669388e7a9a1
Agnieszka Jakubiec <agnieszkaj801gmail.com>
2025-06-03 20:11:19 +0100

    Add Slider

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -13,6 +13,7 @@ class AddOpinionPageContent extends StatefulWidget {
 class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
   var sporthallName = '';
   var fieldName = '';
+  var rating = 5.0;
 
   @override
   Widget build(BuildContext context) {
@@ -40,12 +41,24 @@ class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
               });
             },
           ),
+          Slider(
+            onChanged: (newValue) {
+              setState(() {
+                rating = newValue;
+              });
+            },
+            value: rating,
+            min: 1.0,
+            max: 6.0,
+            divisions: 10,
+            label: rating.toString(),
+          ),
           ElevatedButton(
             onPressed: () async {
               FirebaseFirestore.instance.collection('sports halls').add({
                 'name': sporthallName,
                 'field': fieldName,
-                'rating': 9.0,
+                'rating': rating,
               });
             },
             child: const Text('Dodaj'),
